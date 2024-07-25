bf4c22e49e11eb80ec6f34ff28bd3ed070577c8e
surajbhoji1208 <60956130+surajbhoji1208@users.noreply.github.com>
2024-07-25 10:28:01 +0530

    create react elemte using js

---
+++ app.js
@@ -0,0 +1,26 @@
+{
+  /* <div id="parent1">
+    <div id="child2">
+        <h1>hiii</h1>
+        <h1>hello</h1>
+    </div>
+</div>
+<div id="parent2">
+    <div id="child2">
+        <h1>hiii</h1>
+        <h1>hello</h1>
+    </div>
+</div> */
+}
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", { id: "heading" }, "hiii"),React.createElement("h1", { id: "heading" }, "hello")]
+  )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
