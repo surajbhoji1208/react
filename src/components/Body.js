f8e453e12d734999bd57cd699c7dae083856815b
surajbhoji1208 <60956130+surajbhoji1208@users.noreply.github.com>
2024-08-06 09:59:53 +0530

    5_1.33_Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,8 +4,8 @@ import resData from "../utils/mockData"
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">
-        search
+      <div className="filter">
+       <button className="filter-btn">To rated restorent</button>
       </div>
       <div className="res-container">
         {resData.map((res) => (
