92959f9a5e6ee9f5cfd93f6ba741801bf3a63bdf
surajoneclick <133658509+surajoneclick@users.noreply.github.com>
2024-10-16 19:28:49 +0530

    11_1.32 added_button and onclick event

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -7,16 +7,23 @@ const ItemList = ({ items }) => {
       {items.itemCards.map((item) => (
         <div
           key={item.card.info.id}
-          className="p-2 m-2 border border-gray-200 border-b-2 text-left flex justify-between"
+          className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
         >
-          <div>
-            <div className="p-2">
+          <div className="w-9/12">
+            <div className="py-2">
               <span>{item.card.info.name}</span>
               <span>Rs {item.card.info.price / 100}</span>
             </div>
             <p className="text-xs">{item.card.info.description}</p>
           </div>
-          <img src={CDN_URL + item.card.info.imageId} className="w-14" />
+          <div className="w-3/12 p-4" >
+            <div className="absolute">
+              <button className="p-2 mx-16 rounded-lg bg-black text-white shadow-lg">
+                Add+
+              </button>
+            </div>
+            <img src={CDN_URL + item.card.info.imageId} className="w-full" />
+          </div>
         </div>
       ))}
     </div>
--- src/components/RestaurantCatagory.js
+++ src/components/RestaurantCatagory.js
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({ data }) => {
+  const [showItems,setShowItems] = useState(false)
+  const handleClick = ()=>{
+    setShowItems(!showItems)
+  }
   return (
     <div>
       {
         <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 ">
-          <div className="flex justify-between">
+          <div className="flex justify-between cursor-pointer" onClick={handleClick}>
             <span>{data.card.card.title}</span>
             <span className="font-bold text-lg">Arrow</span>
           </div>
-          <ItemList items={data.card.card}></ItemList>
+         {showItems && <ItemList items={data.card.card}></ItemList>}
         </div>
       }
     </div>
